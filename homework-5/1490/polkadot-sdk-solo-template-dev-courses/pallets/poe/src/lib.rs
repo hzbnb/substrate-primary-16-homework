f13ca066afca3861fe6b03d194fc8187fd01f027
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2024-12-29 20:28:52 +0800

    1490 finished task5

---
+++ homework-5/1490/polkadot-sdk-solo-template-dev-courses/pallets/poe/src/lib.rs
@@ -0,0 +1,135 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use frame_support::pallet_prelude::*;
+use frame_system::pallet_prelude::*;
+pub use pallet::*;
+// pub use weights::WeightInfo;
+
+#[cfg(test)]
+mod mock;
+
+// This module contains the unit tests for this pallet.
+// Learn about pallet unit testing here: https://docs.substrate.io/test/unit-testing/
+#[cfg(test)]
+mod tests;
+
+// All pallet logic is defined in its own module and must be annotated by the `pallet` attribute.
+#[frame_support::pallet]
+pub mod pallet {
+    // Import various useful types required by all FRAME pallets.
+    use super::*;
+
+    #[pallet::pallet]
+    pub struct Pallet<T>(_);
+
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        #[pallet::constant]
+        type MaxClaimLength: Get<u32>;
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+        // type WeightInfo: WeightInfo;
+    }
+
+    #[pallet::storage]
+    pub type Proofs<T: Config> = StorageMap<
+        _,
+        Blake2_128Concat,
+        BoundedVec<u8, T::MaxClaimLength>,
+        (T::AccountId, BlockNumberFor<T>),
+    >;
+
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        /// A user has successfully set a new value.
+        ClaimCreated(T::AccountId, BoundedVec<u8, T::MaxClaimLength>),
+        ClaimRevoked(T::AccountId, BoundedVec<u8, T::MaxClaimLength>),
+    }
+
+    #[pallet::error]
+    pub enum Error<T> {
+        ProofAlreadyExists,
+        ClaimTooLong,
+        ClaimNotExist,
+        NotClaimOwner,
+    }
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        /// An example dispatchable that takes a single u32 value as a parameter, writes the value
+        /// to storage and emits an event.
+        ///
+        /// It checks that the _origin_ for this call is _Signed_ and returns a dispatch
+        /// error if it isn't. Learn more about origins here: <https://docs.substrate.io/build/origins/>
+        #[pallet::call_index(0)]
+        #[pallet::weight(0)]
+        // #[pallet::weight(T::WeightInfo::create_claim(claim.len() as u32))]
+        pub fn create_claim(
+            origin: OriginFor<T>,
+            claim: BoundedVec<u8, T::MaxClaimLength>,
+        ) -> DispatchResult {
+            // Check that the extrinsic was signed and get the signer.
+            let sender = ensure_signed(origin)?;
+            ensure!(
+                !Proofs::<T>::contains_key(&claim),
+                Error::<T>::ProofAlreadyExists
+            );
+
+            Proofs::<T>::insert(
+                &claim,
+                (sender.clone(), frame_system::Pallet::<T>::block_number()),
+            );
+
+            // Emit an event.
+            Self::deposit_event(Event::ClaimCreated(sender, claim));
+
+            // Return a successful `DispatchResult`
+            Ok(())
+        }
+
+        #[pallet::call_index(1)]
+        #[pallet::weight(0)]
+        // #[pallet::weight(T::WeightInfo::revoke_claim(claim.len() as u32))]
+        pub fn revoke_claim(
+            origin: OriginFor<T>,
+            claim: BoundedVec<u8, T::MaxClaimLength>,
+        ) -> DispatchResultWithPostInfo {
+            let sender = ensure_signed(origin)?;
+
+            let (owner, _) = Proofs::<T>::get(&claim).ok_or(Error::<T>::ClaimNotExist)?;
+
+            ensure!(owner == sender, Error::<T>::NotClaimOwner);
+
+            Proofs::<T>::remove(&claim);
+
+            Self::deposit_event(Event::ClaimRevoked(sender, claim));
+
+            Ok(().into()) // 撤销存证功能完成
+        }
+
+        #[pallet::call_index(2)]
+        #[pallet::weight(0)]
+        // #[pallet::weight(T::WeightInfo::transfer_claim(claim.len() as u32))]
+        pub fn transfer_claim(
+            origin: OriginFor<T>,
+            claim: BoundedVec<u8, T::MaxClaimLength>,
+            new_owner: T::AccountId,
+        ) -> DispatchResult {
+            let sender = ensure_signed(origin)?;
+
+            let (owner, _) = Proofs::<T>::get(&claim).ok_or(Error::<T>::ClaimNotExist)?;
+
+            ensure!(owner == sender, Error::<T>::NotClaimOwner);
+            ensure!(new_owner != owner, Error::<T>::NotClaimOwner);
+
+            Proofs::<T>::insert(
+                &claim,
+                (new_owner.clone(), frame_system::Pallet::<T>::block_number()),
+            );
+
+            Self::deposit_event(Event::ClaimCreated(new_owner, claim));
+
+            Ok(())
+        }
+    }
+}
